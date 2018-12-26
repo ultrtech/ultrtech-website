b4f2077e275d2014c31730243e1d0408facef8f2
NikiGamePlay <nikigameplay@outlook.com>
2018-12-26 02:15:51 +0100

    created 'Work in progress' screen

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Header extends React.Component {
+    render(){
+        return(
+            <div className="jumbotron">
+                <h1 className="centered">
+                    Page in construction, come back later!
+                </h1>
+            </div>
+        );
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <Header />,
+    document.getElementById('root')
+  );
