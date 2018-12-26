1c211e397d7d66263de33355125d9a38d9805de9
NikiGamePlay <nikigameplay@outlook.com>
2018-12-26 01:37:20 +0100

    Updates

---
+++ precache-manifest.0b038f181e375daae4d63d1796c8560b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ultrtech-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a25e72cb331caa72d04c",
+    "url": "/ultrtech-website/static/js/runtime~main.a25e72cb.js"
+  },
+  {
+    "revision": "714277584b3534eb6cca",
+    "url": "/ultrtech-website/static/js/main.71427758.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/ultrtech-website/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "714277584b3534eb6cca",
+    "url": "/ultrtech-website/static/css/main.244ec96e.chunk.css"
+  },
+  {
+    "revision": "d17f4787ae136915afa5daee90f97569",
+    "url": "/ultrtech-website/index.html"
+  }
+];
